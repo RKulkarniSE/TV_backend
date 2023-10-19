75712b311e50de824348e3ddd86c0e0a3353ada0
RKulkarniSE <raj.kulkarni@siemens-energy.com>
2023-10-19 16:30:33 +1100

    added relevant routes for footprint data

--- code/TV2/TV2_dash.py
+++ code/TV2/TV2_dash.py
@@ -1,16 +1,9 @@
 import pandas as pd
 
-"""
-Convert the reports csv file into a pandas dataframe
-
-Extract and sort relevant data
-Status, Ticket ID, Account, Product , Date Submitted, Assignees
-"""
 df = pd.read_csv('./report_test.csv', encoding="ISO-8859-1")
 
 def returnColumn(column):
     dfStatus = df[column]
     return dfStatus.to_json()
 
-print(returnColumn("Date Submitted"))
 
--- code/query.py
+++ code/query.py
@@ -3,13 +3,13 @@ from flask_cors import CORS
 import sched
 import time
 from TV1.TV1_dash import DataframeConversion, run_periodically
-
+from TV2.TV2_dash import returnColumn
 app = Flask(__name__)
 cors = CORS(app)
 
 
 scheduler = sched.scheduler(time.time, time.sleep)
-interval = 300
+SCADAinterval = 300
 
 @app.route('/DUID')
 def returnDUID():
@@ -17,12 +17,35 @@ def returnDUID():
 
 @app.route('/SCADA')
 def returnSCADA():
-    return run_periodically(scheduler, interval)
+    return run_periodically(scheduler, SCADAinterval)
 
 @app.route('/CAPACITY')
 def returnCAP():
     return DataframeConversion('Generator Capacity (MW)')
 
+@app.route('/FOOTPRINTS/ASSIGNEES')
+def returnAssignees():
+    return returnColumn('Assignees')
+
+@app.route('/FOOTPRINTS/SUBMITTED')
+def returnSubmission():
+    return returnColumn('Date Submitted')
+
+@app.route('/FOOTPRINTS/STATUS')
+def returnStatus():
+    return returnColumn('Status')
+
+@app.route('/FOOTPRINTS/TICKET')
+def returnTicketNo():
+    return returnColumn('Global Ticket No.')
+
+@app.route('/FOOTPRINTS/ACCOUNT')
+def returnAccount():
+    return returnColumn('Account')
+
+@app.route('/FOOTPRINTS/PRODUCT')
+def returnProduct():
+    return returnColumn('Product')
 
 if __name__ == '__main__':
     app.run(port=8080)
