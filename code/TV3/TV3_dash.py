17ee8dd4fa415b35ae82f5a0cee70742c5fbb650
RKulkarniSE <raj.kulkarni@siemens-energy.com>
2023-11-02 16:58:59 +1100

    notes/updated guidelines

--- code/TV3/TV3_dash.py
+++ code/TV3/TV3_dash.py
@@ -1,13 +1,5 @@
 # KPI page backend
 
-"""
-Functions for collecting
-Ticket per Account and Product
-
-Number of tickets by status
-
-"""
-
 # iterate through folders in Footprints Data and store in a dataframe
 """
 Date (YY/MM)    Account     Product     Status
