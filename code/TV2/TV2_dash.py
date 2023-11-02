08dbf418598f03df08e6b746cb468b60d3864511
RKulkarniSE <raj.kulkarni@siemens-energy.com>
2023-11-02 14:03:14 +1100

    feature/searches for latest report

--- code/TV2/TV2_dash.py
+++ code/TV2/TV2_dash.py
@@ -2,15 +2,18 @@ import pandas as pd
 import os
 import glob
 
-dir_path = "Footprint Data/Ticket Daily Report/2023"
+folder_path = "Footprints Data/Ticket Daily Report"
+folders = os.listdir(folder_path)
+latest_folder = max(folders)
+
+dir_path = f"{folder_path}/{latest_folder}"
 files = os.listdir(dir_path)
 all_csv_files = glob.glob(os.path.join(dir_path, "*.csv"))
 all_csv_files.sort(key=os.path.getmtime)
+latest_report = all_csv_files[-1]
 
-df = pd.read_csv(all_csv_files[-1], encoding="ISO-8859-1")
+df = pd.read_csv(latest_report, encoding="ISO-8859-1")
 
 def returnColumn(column):
     dfStatus = df[column]
     return dfStatus.to_json()
-
-
