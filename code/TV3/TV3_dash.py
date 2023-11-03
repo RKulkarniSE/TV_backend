67fbc2c17874c0a06ab807606c0415d6e711ea0a
RKulkarniSE <raj.kulkarni@siemens-energy.com>
2023-11-03 14:22:15 +1100

    Formed logic for building footprints dataframe

--- code/TV3/TV3_dash.py
+++ code/TV3/TV3_dash.py
@@ -11,6 +11,7 @@ Date (YY/MM)    Account     Product     Status
 footprints_path = "code/Footprints Data/Ticket Daily Report"
 all_folders = os.listdir(footprints_path)
 li = []
+dates_column = []
 for folder in all_folders:
     folder_path = os.listdir(f"{footprints_path}/{folder}")
     for file in folder_path:
@@ -20,9 +21,16 @@ for folder in all_folders:
             os.remove(f"{footprints_path}/{folder}/{file}")
             
         else:
+            
+            report_date = file[:10]
+            dates_column.append(report_date)
+            dates_df = pd.DataFrame(dates_column)
             df = pd.read_csv(f"{footprints_path}/{folder}/{file}", encoding="ISO-8859-1")
+            df = pd.concat([df, dates_df], axis=1)
+            df.dropna(inplace=True)
+            df.rename(columns={0:"Date Created"}, inplace=True)
             li.append(df)
 
 frame = pd.concat(li)
-print(frame.head())
+print(frame.iloc[:100, :10])
 
